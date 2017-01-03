752b9d3d28ee6b9792b330225131cb79ee5271a0
David Bernard <david.bernard.31@gmail.com>
2017-01-03 10:00:10 +0100

    include assimp headers

--- assimp4java/src/main/headers/javacpp-code.h
+++ assimp4java/src/main/headers/javacpp-code.h
@@ -1,3 +1,6 @@
-#include <assimp/Importer.hpp>      // C++ importer interface
-#include <assimp/scene.h>           // Output data structure
-#include <assimp/postprocess.h>
+//#include <assimp/Importer.hpp>      // C++ importer interface
+#include "assimp/Importer.hpp"
+//#include <assimp/scene.h>           // Output data structure
+#include "assimp/scene.h"
+//#include <assimp/postprocess.h>
+#include "assimp/postprocess.h"
